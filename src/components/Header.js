9fc2f750c31108d16e3ae23f89aa3969363f551a
UMAtaullin <ataullinural@gmail.com>
2024-06-05 16:05:21 +0500

    init

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import cls from './Header.module.css'
+
+const Header = () => {
+    return (
+        <header>
+            <div className={cls.logo}>UMA</div>
+        </header>
+    )
+}
+
+export default Header
