02b3ecd7cc5d89040e947c04f03a66a630688bda
UMAtaullin <ataullinural@gmail.com>
2024-06-09 05:09:27 +0500

    try css grid for Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import cls from './Dialogs.module.css'
 
 const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={cls.dialogs_page}>
+      <div className={cls['dialogs_item']}>
+        <div>Земфира</div>
+        <div>Раушания</div>
+        <div>Мансур</div>
+      </div>
+      <div className={cls['messages_item']}>
+        <div>Я приеду в понедельник в 4 утра</div>
+        <div>Сегодня ничего не делала, но очень устала</div>
+        <div>Без понятия что сказать</div>
+      </div>
     </div>
   )
 }
