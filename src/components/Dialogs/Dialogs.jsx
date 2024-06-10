2248061279502cc61c5b828227cbc9d3c4a4597c
UMAtaullin <ataullinural@gmail.com>
2024-06-10 16:03:49 +0500

    dialogsElement and messageElement

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import cls from './Dialogs.module.css'
 import Dialog from './Dialog/Dialog'
 import Message from './Message/Message'
@@ -9,26 +9,27 @@ const Dialogs = (props) => {
     { id: 2, name: 'Мансур' },
     { id: 3, name: 'Раушания' },
   ]
+  let dialogsElement = [
+    <Dialog name={dialogsData[0].name} id={dialogsData[0].id} />,
+    <Dialog name={dialogsData[1].name} id={dialogsData[1].id} />,
+    <Dialog name={dialogsData[2].name} id={dialogsData[2].id} />,
+  ]
   let messageData = [
     { id: 1, message: 'Я приеду в понедельник в 4 утра' },
     { id: 2, message: 'Ничего не делала, но очень устала' },
     { id: 3, message: 'Без понятия что сказать' },
   ]
+  let messageElement = [
+    <Message message={messageData[0].message} />,
+    <Message message={messageData[1].message} />,
+    <Message message={messageData[2].message} />,
+  ]
   return (
     <div className={cls.dialogs_page}>
-      <div className={cls.dialogs}>
-        <Dialog name={dialogsData[0].name} id={dialogsData[0].id} />
-        <Dialog name={dialogsData[1].name} id={dialogsData[1].id} />
-        <Dialog name={dialogsData[2].name} id={dialogsData[2].id} />
-      </div>
-      <div className={cls.messages}>
-        <Message message={messageData[0].message} />
-        <Message message={messageData[1].message} />
-        <Message message={messageData[2].message} />
-      </div>
+      <div className={cls.dialogs}>{dialogsElement}</div>
+      <div className={cls.messages}>{messageElement}</div>
     </div>
   )
 }
 
 export default Dialogs
-
