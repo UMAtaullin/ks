3281b1eb601c0113114718863dc61bd7fafbf71d
UMAtaullin <ataullinural@gmail.com>
2024-06-14 10:48:51 +0500

    use createRef

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,6 +4,15 @@ import cls from './MyPosts.module.css'
 
 const MyPosts = (props) => {
 
+  let newPostEl = React.createRef()
+
+  const AddPost = () => {
+    // let text = document.getElementById('new-post').value
+    let text = newPostEl.current.value
+    alert(text)
+  }
+  const DeletePost = () => alert('Удалить сообщение?')
+
   let postElement = props.posts.map((el) => (
     <Post message={el.message} like={el.like} />
   ))
@@ -13,17 +22,16 @@ const MyPosts = (props) => {
       New post
       <div className={cls.posts}>
         <div className={cls.new}>
-          <textarea className={cls.text}></textarea>
+          {/* <textarea id='new-post' className={cls.text}></textarea> */}
+          <textarea ref={newPostEl} className={cls.text}></textarea>
         </div>
         <div className={cls.btn}>
-          <button 
-          onClick={() => {alert('Добавить сообщение?')}}
-          className={cls.add}>
+          <button onClick={AddPost} className={cls.add}>
             Add post
           </button>
-          <button 
-          onClick={()=>{alert('Удалить сообщение?')}}
-          className={cls.delete}>Delete</button>
+          <button onClick={DeletePost} className={cls.delete}>
+            Delete
+          </button>
         </div>
       </div>
       <div className={cls.my}>My posts</div>
